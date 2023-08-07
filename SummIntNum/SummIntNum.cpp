0c30ddcb2fa208d2d6337a6858a0a6d7cd1f76b2
Gridin_Andrei <andreigridin918@gmail.com>
2023-08-07 20:29:50 +0500

    Добавьте файлы проекта.

---
+++ SummIntNum/SummIntNum.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int SummIntNumm(int num) {
+    double result = (double)(1 + num) / 2 * num;
+    /*while (num > 0) {
+        result += num;
+        num--;
+    }*/
+    return result;
+}
+
+int main()
+{
+    system("chcp 1251 > nul");
+    int nummer;
+    cout << "Введите натуральное число: ";
+    cin >> nummer;
+    cout << "Сумма натуральных чисел до " << nummer << " составляет " << SummIntNumm(nummer);
+    system("pause > nul");
+    return 0;
+}
